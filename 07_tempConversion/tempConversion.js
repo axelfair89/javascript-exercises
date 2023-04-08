215a8ba2dc6050aa833b836367537831dd38186c
axelfair89 <67049816+axelfair89@users.noreply.github.com>
2023-04-08 17:37:55 +1000

    temp conversion exericse

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  let celci = (far - 32) * (5 / 9);
+  return Math.round(celci * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let farren = cel * (9 / 5) + 32;
+  return Math.round(farren * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
